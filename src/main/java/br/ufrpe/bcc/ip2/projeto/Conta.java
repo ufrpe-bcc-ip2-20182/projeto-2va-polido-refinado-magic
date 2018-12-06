f3b58b7018a7ba955d224f5cc2e0704a3a299c28
Alcides Cunha <alcides.cunha@ufrpe.br>
2018-12-05 22:33:00 -0300

    Criação dos primeiros atributos

---
+++ src/main/java/br/ufrpe/bcc/ip2/projeto/Conta.java
@@ -0,0 +1,19 @@
+package br.ufrpe.bcc.ip2.projeto;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Conta
+{
+    private String login;
+    private String senha;
+    private Date dataDeCriacao;
+    private double saldo;
+    private boolean status;
+    //private ArrayList<Boletos> boletos;
+
+    public Conta()
+    {
+
+    }
+}
