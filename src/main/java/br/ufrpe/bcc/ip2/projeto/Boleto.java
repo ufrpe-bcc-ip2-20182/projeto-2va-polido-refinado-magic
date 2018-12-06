81a117f3e1b7e7b4babbfe7ff5237f913584304e
Victor Vidal <victor.vidal@ufrpe.br>
2018-12-05 23:49:38 -0200

    Criação da classe Boleto, seus atributos, getters e setters

---
+++ src/main/java/br/ufrpe/bcc/ip2/projeto/Boleto.java
@@ -0,0 +1,52 @@
+package br.ufrpe.bcc.ip2.projeto;
+import java.util.Date;
+
+public class Boleto
+{
+    private String nomeDoBoleto;
+    private Date dataDeCriacao;
+    private Date dataDeVencimento;
+    private boolean pagamento;
+    private double valor;
+
+
+    public String getNomeDoBoleto() {
+        return nomeDoBoleto;
+    }
+
+    public void setNomeDoBoleto(String nomeDoBoleto) {
+        this.nomeDoBoleto = nomeDoBoleto;
+    }
+
+    public Date getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public void setDataDeCriacao(Date dataDeCriacao) {
+        this.dataDeCriacao = dataDeCriacao;
+    }
+
+    public Date getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(Date dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public boolean isPagamento() {
+        return pagamento;
+    }
+
+    public void setPagamento(boolean pagamento) {
+        this.pagamento = pagamento;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
