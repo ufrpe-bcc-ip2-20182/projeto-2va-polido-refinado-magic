bcfef415fca3eab04003e8f8e4b247cfb90c6bb2
Alcides Cunha <alcides.cunha@ufrpe.br>
2018-12-08 12:33:57 -0300

    Tratando erro com double

--- src/main/java/br/ufrpe/bcc/ip2/projeto/Main.java
+++ src/main/java/br/ufrpe/bcc/ip2/projeto/Main.java
@@ -1,12 +1,43 @@
 package br.ufrpe.bcc.ip2.projeto;
 
+import java.text.ParseException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args)
     {
         System.out.println("Executando...");
         Conta conta = new Conta();
-        conta.cadastro();
+        //conta.cadastro();
+        boolean x = Verif.cpfcerto("123.456.789-10");
+        boolean controller = true;
+        Scanner scan = new Scanner(System.in);
+        while(true)
+        {
+            controller = true;
+            try
+            {
+                double num = scan.nextDouble();
+            }
+            catch (InputMismatchException e)
+            {
+                System.out.println("Você digitou algo errado!");
+                controller = false;
+                scan.next();
+                System.out.println("Tente de novo!");
+            }
+            finally
+            {
+                if(controller == true)
+                {
+                    System.out.println("Tudo certo!");
+                    break;
+                }
+            }
+        }
+        //System.out.println(x);
         //para poder fazer o login, precisamos verificar algum tipo de array de contas
         //a mesma coisa para o cadastro, tem que ter algum tipo de array de contas
     }
--- src/main/java/br/ufrpe/bcc/ip2/projeto/Verif.java
+++ src/main/java/br/ufrpe/bcc/ip2/projeto/Verif.java
@@ -1,10 +1,40 @@
 package br.ufrpe.bcc.ip2.projeto;
 
+import java.util.*;
+import java.lang.String;
+import java.lang.Object.*;
+
 public abstract class Verif
 {
     //essa classe tem a função de conferir problemas e resolver coisas em geral que podem dar errado
-    public static void branco()
+    public static boolean branco(String texto)
     {
+        if(texto.isEmpty())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
+    public static boolean cpfcerto(String texto)
+    {
+        //ta printando errado
+        String cortado[] = new String[texto.length()];
+        for(int i = 0; i < texto.length(); i++)
+        {
+            cortado[i] = texto.substring(i,i+1);
+            System.out.println("Posição " + i + ": " + cortado[i]);
+        }
+        if((cortado[3] == ".") && (cortado[7] == ".") && (cortado[11] == "-"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
