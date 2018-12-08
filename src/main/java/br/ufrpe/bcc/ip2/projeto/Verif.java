9c2ed22d2c25acc7a2a4942e2c80ce45f31c2f2d
Alcides Cunha <alcides.cunha@ufrpe.br>
2018-12-07 21:25:08 -0300

    Iniciação da classe Verif

---
+++ src/main/java/br/ufrpe/bcc/ip2/projeto/Verif.java
@@ -0,0 +1,10 @@
+package br.ufrpe.bcc.ip2.projeto;
+
+public abstract class Verif
+{
+    //essa classe tem a função de conferir problemas e resolver coisas em geral que podem dar errado
+    public static void branco()
+    {
+
+    }
+}
