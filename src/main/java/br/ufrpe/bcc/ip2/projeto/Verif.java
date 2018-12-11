70e9c1271dbb1bfde1b6dcaf8afd677d2c4515a4
Victor Vidal <victor.vidal@ufrpe.br>
2018-12-10 23:50:46 -0200

    cpfcerto2

--- src/main/java/br/ufrpe/bcc/ip2/projeto/Verif.java
+++ src/main/java/br/ufrpe/bcc/ip2/projeto/Verif.java
@@ -84,6 +84,40 @@ public abstract class Verif
              return true;
       }*/
 
+    public  static boolean cpfcerto2(String texto)
+    {
+        // AI ESSE CARALHO
+        String caracteres[] = new String[texto.length()];
+
+        for(int i=0;i<texto.length();i++)
+        {
+            caracteres[i] = texto.substring(i,i+1);
+        }
+
+
+        for(int i=0;i<texto.length();i++)
+        {
+            int aux=0;
+            for(int j=0;j<=9;j++)
+            {
+                if( caracteres[i].equals(String.valueOf(j)))
+                {
+                    aux++;
+                }
+            }
+            if(aux==0) return false;
+        }
+
+        if( (caracteres[3].equals(".")) && (caracteres[7].equals(".")) && (caracteres[11].equals("-")) )
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
     public static double pegarDouble()
     {
         boolean controller = true;
