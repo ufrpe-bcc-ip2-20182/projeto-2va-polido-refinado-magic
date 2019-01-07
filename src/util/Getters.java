1c8a96c2deb09205e3b0f2c134a53623facdf662
Alcides Cunha <alcides.cunha@ufrpe.br>
2019-01-06 22:28:18 -0300

    Alteração no esquema de datas (agora finalmente funcional)

--- src/util/Getters.java
+++ src/util/Getters.java
@@ -17,22 +17,93 @@ public class Getters
         return dataDeHoje;
     }
 
+    public static String converteData(Date data) throws ParseException
+    {
+        Date hoje = data;
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        return formatter.format(hoje);
+    }
+
     public static Date converterData(String data)
     {
         String[] partes = Verif.cortarEspacos(data);
-        data = partes[1] + "/" + partes[2] + "/" + partes[5];
+        int h = 0;
+        switch (partes[1])
+        {
+            case "Jan":
+                h = 1;
+                break;
+
+            case "Fev":
+                h = 2;
+
+                break;
+
+            case "Mar":
+                h = 3;
+                break;
+
+            case "Apr":
+                h = 4;
+                break;
+
+            case "May":
+                h = 5;
+                break;
+
+            case "Jun":
+                h = 6;
+                break;
+
+            case "Jul":
+                h = 7;
+                break;
+
+            case "Aug":
+                h = 8;
+                break;
+
+            case "Sep":
+                h = 9;
+                break;
+
+            case "Oct":
+                h = 10;
+                break;
+
+            case "Nov":
+                h = 11;
+                break;
+
+            case "Dec":
+                h = 12;
+                break;
+
+            default:
+                break;
+
+        }
+        if(h < 10)
+        {
+            data = partes[2] + "/" + "0" + h + "/" + partes[5];
+        }
+        else
+        {
+            data = partes[2] + "/" + h + "/" + partes[5];
+        }
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
         Date dataretornada = null;
-
+        System.out.println(data);
         try
         {
             dataretornada = dateFormat.parse(data);
         }
         catch (ParseException x)
         {
-            //Ignora amore :)
+            System.out.println(x);
         }
+
         return dataretornada;
     }
 
