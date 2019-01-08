d7f5110d497b28d9d4dd568bfe550f78e5ea54b4
fernandaalbuquerque <fernandaocalbuquerque@gmail.com>
2019-01-08 00:06:15 -0300

    Adicionar pagamento botao e field

--- src/UI/controller/AddController.java
+++ src/UI/controller/AddController.java
@@ -3,10 +3,22 @@ package UI.controller;
 import UI.MainJavaFX;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
 public class AddController {
-    @FXML
-    protected void btVoltar(ActionEvent e){
+    @FXML protected void btVoltar(ActionEvent e){
         MainJavaFX.changeScreen("logado");
     }
+
+    @FXML private Button addButton;
+    @FXML private TextField nomeField;
+    @FXML private TextField dataDoPagamentoField;
+    @FXML private TextField valorPagField;
+    @FXML private CheckBox pagoBox;
+    @FXML private CheckBox aPagarBox;
+
+
+
 }
