9dcee71369e65bf0a83428fe3f26a540c330c656
Victor Vidal <victor.vidal@ufrpe.br>
2019-01-11 23:14:52 -0200

    Implementação da Classe AddController

--- src/GUI/controller/AddController.java
+++ src/GUI/controller/AddController.java
@@ -3,8 +3,12 @@ package GUI.controller;
 import GUI.MainJavaFX;
 import arquivo.Arquivo;
 import arquivo.LerArquivo;
+import arquivo.SalvarArquivo;
+import beans.Boleto;
 import beans.Conta;
 import beans.Sistema;
+import controller.ArquivoContas;
+import controller.LerConta;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -14,7 +18,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import util.DateUtils;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class AddController {
@@ -36,7 +44,7 @@ public class AddController {
     private LerArquivo lerArquivo = new LerArquivo();
     private ArrayList<Conta> contas = new ArrayList<>();
     private ArrayList<String> logins = new ArrayList<>();
-    Boolean pagamento;
+    private boolean pagamento;
 
     public void initialize(){
         sistema = lerArquivo.ler(arquivo.getArquivo());
@@ -56,22 +64,64 @@ public class AddController {
         Stage stage = null;
         Parent root = null;
 
+        int indice = -1;
+
+        ArquivoContas arquivoContas = new ArquivoContas();
+        sistema = LerArquivo.ler(arquivo.getArquivo());
+
+        try
+        {
+            indice = LerConta.ler(arquivoContas.getArquivo());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
         String nome = nomeField.getText();
+        boolean nomeOk = false;
+        if( (!nome.equals("<NOVA-CONTA>")) && (!nome.equals("NOVO-BOLETO>")) && (!nome.equals("<FIM-BOLETO>")) && (!nome.equals("<FIM-CONTA>")) && (!nome.equals("<FIM-ARQUIVO>")) )
+        {
+            nomeOk = true;
+        }
+        else
+        {
+            Alert alert1 = new Alert(Alert.AlertType.ERROR);
+            alert1.setTitle("Falha ao Adicionar Pagamento");
+            alert1.setHeaderText("Informações inválidas");
+            alert1.setContentText("Nome Inválido");
+            alert1.showAndWait();
+        }
 
-        Double valor = Double.parseDouble(valorPagField.getText());
+        double valor = Double.parseDouble(valorPagField.getText());
+        boolean valorOk = false;
+        if(valor>0)
+        {
+            valorOk = true;
+        }
+        else
+        {
+            Alert alert1 = new Alert(Alert.AlertType.ERROR);
+            alert1.setTitle("Falha ao Adicionar Pagamento");
+            alert1.setHeaderText("Informações inválidas");
+            alert1.setContentText("Valor Inválido");
+            alert1.showAndWait();
+        }
 
-
-        if(pagoBox.isSelected()==true)
+        boolean pagoBoxOk = false;
+        if(pagoBox.isSelected())
         {
             pagamento = true;
+            pagoBoxOk = true;
         }
 
-        if(aPagarBox.isSelected()==true)
+        if(aPagarBox.isSelected())
         {
             pagamento = false;
+            pagoBoxOk = true;
         }
 
-        if( (pagoBox.isSelected()==true) && (aPagarBox.isSelected()==true) )
+        if( (pagoBox.isSelected()) && (aPagarBox.isSelected()) )
         {
             Alert alert2 = new Alert(Alert.AlertType.ERROR);
             alert2.setTitle("Falha ao Adicionar");
@@ -80,5 +130,49 @@ public class AddController {
             alert2.showAndWait();
         }
 
+        SimpleDateFormat f= new SimpleDateFormat("dd/MM/yyyy");
+        java.util.Date data = null;
+        java.util.Date data2 = null;
+        boolean dataFormato = true;
+
+        try {
+            data2 = f.parse(dataDoPagamentoField.getText());
+            data = f.parse(DateUtils.getDiadeHoje());
+        } catch (ParseException e) {
+            dataFormato = false;
+            Alert alert1 = new Alert(Alert.AlertType.ERROR);
+            alert1.setTitle("Falha de Cadastro");
+            alert1.setHeaderText("Informações inválidas");
+            alert1.setContentText("Data contém formatação inválida");
+            alert1.showAndWait();
+        }
+
+        if( (nomeOk) && (valorOk) && (pagoBoxOk) && (dataFormato) )
+        {
+            Boleto novoBoleto = new Boleto();
+
+            novoBoleto.setNomeDoBoleto(nome);
+            novoBoleto.setValor(valor);
+            novoBoleto.setPagamento(pagamento);
+            novoBoleto.setDataDeCriacao(data);
+            novoBoleto.setDataDeVencimento(data2);
+
+            double pagar = sistema.getContas().get(indice).getSaldo();
+            if((pagar-valor)>0)
+            {
+                sistema.getContas().get(indice).setSaldo(pagar-valor);
+            }
+            else
+            {
+                Alert alert1 = new Alert(Alert.AlertType.ERROR);
+                alert1.setTitle("Falha ao Adicionar Pagamento");
+                alert1.setHeaderText("Informações inválidas");
+                alert1.setContentText("Saldo da Conta insuficiente");
+                alert1.showAndWait();
+            }
+            sistema.getContas().get(indice).setBoletos(novoBoleto);
+            SalvarArquivo salvarArquivo = new SalvarArquivo();
+            salvarArquivo.salvar(sistema, arquivo.getArquivo());
+        }
     }
 }
