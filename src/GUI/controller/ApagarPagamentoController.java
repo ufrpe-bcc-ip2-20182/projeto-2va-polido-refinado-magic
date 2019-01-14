475416f2ec14824ae8cef427a307248afd885508
fernandaalbuquerque <fernandaocalbuquerque@gmail.com>
2019-01-13 21:10:24 -0500

    Tela de Apagar Pagamento

---
+++ src/GUI/controller/ApagarPagamentoController.java
@@ -0,0 +1,14 @@
+package GUI.controller;
+
+import javafx.fxml.FXML;
+import sun.font.TextRecord;
+
+import java.awt.*;
+
+public class ApagarPagamentoController {
+    @FXML private Button apagarPagButton;
+    @FXML private TextField nomeDoPagTF;
+    @FXML private Checkbox apagarTodosCB;
+    @FXML private Checkbox apagarUmCB;
+    @FXML private Button voltarButton;
+}
